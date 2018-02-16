4607c6bdb0c81706a7b5896d0049990915387720
Casey <caseyjacksonking@gmail.com>
2018-02-16 14:33:37 -0500

    updates

---
+++ TestArrays.java
@@ -0,0 +1,39 @@
+
+public class TestArrays {
+
+	public static void main(String[] args) {
+		
+		int [] index = new int[4];
+		String [] islands = new String[4];
+		
+		int y=0;
+		
+		index[0] = 1;
+		index[1] = 3;
+		index[2] = 0;
+		index[3] = 2;
+		
+		
+		
+		
+		islands [0] = "Bermuda";
+		islands [1] = "Fiji";
+		islands [2] = "Azores";
+		islands [3] = "Cozumel";
+		
+		int ref;
+		while (y<4){
+			
+			ref = index[y];
+			y=y+1;
+			
+			System.out.print("island = ");
+			System.out.println(islands[ref]);
+		}
+		
+		
+		
+
+	}
+
+}
