8bac633f36fd0786370cbcdeeb69d9cf26217cca
Casey <caseyjacksonking@gmail.com>
2018-02-22 17:35:58 -0500

    Day 14

---
+++ Day14.java
@@ -0,0 +1,18 @@
+
+public class Day14 {
+
+	public static void main(String[] args) {
+		
+		Student student1 = new Student();
+		student1.firstName = "Casey";
+		student1.lastName = "King;";
+		student1.age = 34;
+		student1.dob = "October 4, 1983";
+		
+		System.out.println ( "Student Name: " + student1.firstName + " " + student1.lastName);
+		System.out.println ("Student Age: " + student1.age);
+		System.out.println ("Student DOB: " + student1.dob);
+
+	}
+
+}
